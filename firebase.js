e986279623ff9d8ea4c5e0019c3fb21b1b0671e1
Vu Hac Lab <vuhaclab@gmail.com>
2023-03-05 09:37:19 +0000

    Update 3 files

--- firebase.js
+++ firebase.js
@@ -1,24 +1,30 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from 'firebase/storage'
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKYeBColkGBaVhH068Fbb5bAj5G3XvKz8",
-  authDomain: "connect-bacc3.firebaseapp.com",
-  projectId: "connect-bacc3",
-  storageBucket: "connect-bacc3.appspot.com",
-  messagingSenderId: "416147349063",
-  appId: "1:416147349063:web:cdddd0d5bbe8c890e85b62",
-  storageBucket: "connect-bacc3.appspot.com"
+  apiKey: "AIzaSyD3Hnl057CAD5iA-ia47fhsiMaoZh1smi4",
+
+  authDomain: "chatapp-vhlab.firebaseapp.com",
+
+  projectId: "chatapp-vhlab",
+
+  storageBucket: "chatapp-vhlab.appspot.com",
+
+  messagingSenderId: "33781491667",
+
+  appId: "1:33781491667:web:cdd1b1cf369418fcdbb126",
+
+  measurementId: "G-KVKX1TT0QW",
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
-const db = getFirestore() 
-const auth = getAuth()
-const storage = getStorage()
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+const storage = getStorage();
 
-export { db, auth, storage }
+export { db, auth, storage };
